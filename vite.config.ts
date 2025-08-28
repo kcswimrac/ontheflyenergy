8ce5a78ecbd48791af821d234a701b01cb0f3e31
oliver nash <75489794+olivernashivy@users.noreply.github.com>
2025-08-28 19:48:20 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/on-the-fly-energy-landing/',
+  // base: '/on-the-fly-energy-landing/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
